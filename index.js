0350eeb0937f496f30cb396f8de5d6d421ee5a5c
Sungoh Park <spark449062@gmail.com>
2021-06-30 03:53:57 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+//removing the 'main' node
+let elementMain = document.getElementsByTagName('main')[0];
+elementMain.remove();
+
+// index.html
+// after index.js is processed
+// has a 'newHeader' variable that points to node 'h1#victory':
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "is the champion"
+document.body.appendChild(newHeader);
